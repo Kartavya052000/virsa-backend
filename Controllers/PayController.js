67f581c7891d66fce7a2ce6e7178b6849e866115
Kartavya Bhayana <kartavyabhayana1@gmail.com>
2024-02-22 22:37:38 -0800

    prod env set

--- Controllers/PayController.js
+++ Controllers/PayController.js
@@ -1,11 +1,10 @@
-//kartavya test
-// const stripe = require('stripe')('sk_test_51ODkV4AH7tWiIHmOZh1w3EY7dSDEMjjXjiFgnN5NSkInnwc4I8wj5s8jNypvM59wHJPRdqcK4pojdMQqGGEPxNs80063qbmqIg');
+require("dotenv").config();
 
-//virsa test
-// const stripe = require('stripe')('sk_test_51OlNSaGFRX0mGJ8pt0b4PJVdj6J13KjPZz3g4JEP8TblhnBClX5VRtWB4MutyBa6isQNavWYT4irFWhHRc9y7Gyl00KIE9BdgY');
 
-// virsa prod
-const stripe = require('stripe')('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');
+
+const {stripe_secret,publish_key}= process.env
+
+const stripe = require('stripe')(stripe_secret);
 
 exports.Pay= async (req, res) => {
   const{amount}=req.body
@@ -32,15 +31,9 @@ exports.Pay= async (req, res) => {
       paymentIntent: paymentIntent.client_secret,
       ephemeralKey: ephemeralKey.secret,
       customer: customer.id,
-      // below is kartavya test
-      // publishableKey: 'pk_test_51ODkV4AH7tWiIHmOF0t9QJE4KMdeXjC86OOcFlIuZ6wX9enYJ2E5LNpCIF1pR5jSmyQL28kr9UFXyyqzXuN7vBUL007BzK9dRh'
-      // below is vira test
-            // publishableKey: 'pk_test_51OlNSaGFRX0mGJ8pvxXdIbNolXrqf2gMXhFTCNV8wILFiFSl2eQ2tXzkQEhOXdrCYieM7s5kOMphjyyddzwWUADA00BR26k7v8'
-// below is virsa prod
-publishableKey: 'pk_live_51OlNSaGFRX0mGJ8phjM6ZEf28ZYFRCHA9bR7AcgsqM7bRkIDch8rJCYqkUd5siNh0AQOMjZtZ0VhQfIUKTXLNo4C00DhAVmA4n'
+
+publishableKey: publish_key
 
     });
-    // kartavya key
-      // publishableKey: 'pk_test_51ODkV4AH7tWiIHmOF0t9QJE4KMdeXjC86OOcFlIuZ6wX9enYJ2E5LNpCIF1pR5jSmyQL28kr9UFXyyqzXuN7vBUL007BzK9dRh'
-      //virsa key
+  
   }
