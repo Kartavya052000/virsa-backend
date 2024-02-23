b707cd9ec486e32b7e76160472cc91d917465ad7
Kartavya Bhayana <kartavyabhayana1@gmail.com>
2024-02-22 22:49:23 -0800

    env set

--- Controllers/PayController.js
+++ Controllers/PayController.js
@@ -3,12 +3,13 @@ require("dotenv").config();
 
 
 const {stripe_secret,publish_key}= process.env
-
 const stripe = require('stripe')(stripe_secret);
 
 exports.Pay= async (req, res) => {
   const{amount}=req.body
   console.log(amount)
+console.log(stripe_secret)
+
     // Use an existing Customer ID if this is a returning customer.
     const customer = await stripe.customers.create();
     const ephemeralKey = await stripe.ephemeralKeys.create(
@@ -16,8 +17,8 @@ exports.Pay= async (req, res) => {
       {apiVersion: '2023-10-16'}
     );
     const paymentIntent = await stripe.paymentIntents.create({
-      // amount: amount*100, // Amount in INR (smallest currency unit)
-      amount: 1000, // Amount in INR (smallest currency unit)
+      amount: amount*100, // Amount in INR (smallest currency unit)
+      // amount: 1000, // Amount in INR (smallest currency unit)
       currency: 'inr',
       customer: customer.id,
       
